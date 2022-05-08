7f59dfe785d2ac7afd5cea9fba102e37285430f0
Wholy <wholyomfg@gmail.com>
2022-05-08 17:15:36 +0100

    Delete survival-start.sh

--- survival-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Check if server is already started
-if screen -list | grep -q "survival"; then
-    echo "Server is already started!  Press screen -r survival to open it"
-    exit 1
-fi
-
-screen -S "survival" -d -m sh survival-server.sh
