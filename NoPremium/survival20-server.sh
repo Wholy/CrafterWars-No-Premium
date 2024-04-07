af442bfb184796852d32fead41e6a6dec1b72700
Wholy <wholyomfg@gmail.com>
2024-04-08 00:07:22 +0100

    Add files via upload

---
+++ NoPremium/survival20-server.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/usr/lib/jvm/java-17-oracle/bin/java -Xms20G -Xmx20G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Dlog4j2.formatMsgNoLookups=true -Daikars.new.flags=true -jar spigot-1.20.1.jar
